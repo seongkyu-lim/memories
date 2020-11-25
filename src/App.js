af9d88c0760cab7ea805d7e40e602bd8f065a244
limseongkyu <96x60812@naver.com>
2020-11-25 14:21:04 +0900

    [Add] img fishbowl, ricecooker

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import styled from "styled-components";
 
 import BackgroundImg from "/Users/limseongkyu/memories/src/Image/background4.jpeg";
 import Book1Img from "/Users/limseongkyu/memories/src/Image/Book.jpeg";
+import Fishbowl from "/Users/limseongkyu/memories/src/Image/fishbowl.jpeg";
+import Ricecooker from "/Users/limseongkyu/memories/src/Image/ricecooker.jpeg";
 
 class App extends React.Component {
   render() {
     return (
       <Container>
         <Book1Image src={Book1Img} />
+        <Fishbowl1 src={Fishbowl} />
+        <Ricecooker1 src={Ricecooker} />
       </Container>
     );
   }
@@ -23,7 +27,14 @@ const Container = styled.div`
   background-image: url(${BackgroundImg});
   background-size: cover;
 `;
-
+const Ricecooker1 = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+const Fishbowl1 = styled.img`
+  width: 200px;
+  height: 200px;
+`;
 const Book1Image = styled.img`
   width: 200px;
   height: 200px;
