2cfbfc0042f5b405eeafa1d4b1902f3334098f9b
limseongkyu <96x60812@naver.com>
2020-12-10 20:07:33 +0900

    [Edit] router

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,4 @@
 export { default as Ricecooker } from "./Ricecooker";
 export { default as Fishbowl } from "./Fishbowl";
 export { default as Book1 } from "./Book1";
+export { default as Main } from "./Main";
--- src/Shared/App.js
+++ src/Shared/App.js
@@ -1,85 +1,30 @@
-import React from "react";
-import styled from "styled-components";
-import { Route, NavLink } from "react-router-dom";
-import { Fishbowl, Ricecooker, Book1 } from "../Pages";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
-import BackgroundImg from "/Users/limseongkyu/memories/src/Image/background4.jpeg";
-import Book1Img from "/Users/limseongkyu/memories/src/Image/Book.jpeg";
-import FishbowlImg from "/Users/limseongkyu/memories/src/Image/fishbowl.jpeg";
-import RicecookerImg from "/Users/limseongkyu/memories/src/Image/ricecooker.jpeg";
+import Book1 from '../Pages/Book1';
+import Fishbowl from '../Pages/Fishbowl';
+import Ricecooker from '../Pages/Ricecooker';
+import Main from '../Pages/Main';
 
 class App extends React.Component {
   render() {
-    const activeStyle = {
-      background: "gray",
-      fontSize: "2rem",
-    };
     return (
-      <Container>
-        <NavLink exact to="/Book1" activeStyle={activeStyle}>
-          <Book1Image src={Book1Img} />
-        </NavLink>
-        <NavLink exact to="/Fishbowl" activeStyle={activeStyle}>
-          <Fishbowl1 src={FishbowlImg} />
-        </NavLink>
-        <NavLink exact to="/Ricecooker" activeStyle={activeStyle}>
-          <Ricecooker1 src={RicecookerImg} />
-        </NavLink>
-        <Route path="/Book1" component={Book1} />
-        <Route path="/Fishbowl" component={Fishbowl} />
-        <Route path="/Ricecooker" component={Ricecooker} />
-      </Container>
-    );
+      <Router>
+        <Switch>
+          <Route path="/" component={Main} />
+          <Route path="/Book1" component={Book1} />
+          <Route path="/Fishbowl" component={Fishbowl} />
+          <Route path="/Ricecooker" component={Ricecooker} />
+        </Switch>
+      </Router>
+    )
   }
 }
+   
 
-const Container = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-image: url(${BackgroundImg});
-  background-size: cover;
-`;
-const Ricecooker1 = styled.img`
-  position: relative;
-  top: 230px;
-  left: 600px;
-  margin: auto;
-  display: block;
-  width: 120px;
-  height: 120px;
-  transition: all 0.3s ease-in-out;
-  &:hover {
-    transform: scale(1.2);
-  }
-`;
-const Fishbowl1 = styled.img`
-  position: relative;
-  top: 250px;
-  left: 400px;
-  margin: auto;
-  display: block;
-  width: 100px;
-  height: 100px;
-  transition: all 0.3s ease-in-out;
-  &:hover {
-    transform: scale(1.2);
-  }
-`;
-const Book1Image = styled.img`
-  position: fixed;
-  top: 20px;
-  left: 100px;
-  margin: auto;
-  display: block;
-  width: 100px;
-  height: 100px;
-  transition: all 0.3s ease-in-out;
-  &:hover {
-    transform: scale(1.2);
-  }
-`;
-
+   
 export default App;
